f45b4ed99b2772c0d8f47b4778874a86cded5280
Rexhep Kelmendi <rexhep.kelmendi123@gmail.com>
2025-07-13 16:47:28 +0200

    Initialize repository with only cms folder

---
+++ cms/config/database.ts
@@ -0,0 +1,22 @@
+type EnvFunction = {
+  (key: string, defaultValue?: string): string | undefined;
+  bool: (key: string, defaultValue?: boolean) => boolean;
+  int: (key: string, defaultValue?: number) => number;
+};
+
+export default ({ env }: { env: EnvFunction }) => ({
+  connection: {
+    client: 'postgres', 
+  connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapidb'),
+        user: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
+        ssl: env.bool("DATABASE_SSL", false) && {
+          rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+      },
+      },
+      debug: false,
+  },
+});
---
+++ cms/config/server.ts
@@ -0,0 +1,14 @@
+type EnvFunction = {
+  (key: string, defaultValue?: string): string | undefined;
+  bool: (key: string, defaultValue?: boolean) => boolean;
+  int: (key: string, defaultValue?: number) => number;
+  array: (key: string) => string[];
+};
+
+export default ({ env }: { env: EnvFunction }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+});
