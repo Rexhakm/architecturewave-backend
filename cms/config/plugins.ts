f35371eadb6c36a2fa8030ff4dfdf81d9ca68f20
Rexhep Kelmendi <rexhep.kelmendi123@gmail.com>
2025-07-13 22:01:01 +0200

    deploy final

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+export default ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET', 'iE220HUgV6mfQFomynB1ZXGnBmu6H0SJe4t5JtW5lE4='),
+    },
+  },
+});
