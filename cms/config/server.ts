35f7a3ca7cf1b0a32db6b3da358ea864ff769000
Rexhep Kelmendi <rexhep.kelmendi123@gmail.com>
2025-07-13 21:46:53 +0200

    deploy for 1000 here

--- cms/config/server.ts
+++ cms/config/server.ts
@@ -9,6 +9,6 @@ export default ({ env }: { env: EnvFunction }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS') || ['myKeyA', 'myKeyB', 'myKeyC', 'myKeyD'],
   },
 });
