c5460ec4f9926b7e610b37e624b622fcb9b413d1
Rexhep Kelmendi <rexhep.kelmendi123@gmail.com>
2025-07-26 19:52:11 +0200

    optimize images

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -4,4 +4,17 @@ export default ({ env }) => ({
       jwtSecret: env('JWT_SECRET', 'iE220HUgV6mfQFomynB1ZXGnBmu6H0SJe4t5JtW5lE4='),
     },
   },
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+        params: {
+          folder: 'architecture-wave',
+        },
+      },
+    },
+  },
 });
