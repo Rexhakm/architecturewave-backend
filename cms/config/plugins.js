d8d66937c84ce050952d672e67dd89f77d0c8e0a
Rexhep Kelmendi <rexhep.kelmendi123@gmail.com>
2025-07-27 09:28:55 +0200

    publish this one

--- cms/config/plugins.js
+++ cms/config/plugins.js
@@ -11,9 +11,6 @@ module.exports = ({ env }) => ({
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
-        params: {
-          folder: 'architecture-wave',
-        },
       },
     },
   },
